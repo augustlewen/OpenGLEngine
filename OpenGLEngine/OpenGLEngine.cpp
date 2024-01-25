93f56c983b84c145c9d0bd675e4aa00063e394bb
augustlewen <augustlewen@live.com>
2024-01-25 09:38:41 +0100

    added some classes, and made the whole window creation be made in the window class

--- OpenGLEngine/OpenGLEngine.cpp
+++ OpenGLEngine/OpenGLEngine.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "Window.h"
 
 using namespace std;
+
 
 void processInput(GLFWwindow*);
 
@@ -15,44 +17,11 @@ void error_callback(int error, const char* msg) {
     cerr << " [" << error << "] " << msg << endl;
 }
 
+
+
 int main() {
 
-    glfwSetErrorCallback(error_callback);
-
-    // Initialize GLFW
-    if (!glfwInit()) { // Exit, if it failed
-        cout << "Failed to init GLFW" << endl;
-        return -1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#if __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // Request Window from Operating System
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
-    if (window == nullptr)
-    {
-        cout << "Failed to create GLFW window" << endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // Initialize GLAD (connects OpenGL Functions)
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        cout << "Failed to initialize GLAD" << endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
+    Window mainWindow{800, 600};
 
     // Initialization ends here
     // ==================================================================
@@ -163,11 +132,11 @@ int main() {
 
 
     // While the User doesn't want to Quit (X Button, Alt+F4)
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(mainWindow.window))
     {
         // process input (e.g. close window on Esc)
         glfwPollEvents();
-        processInput(window);
+        processInput(mainWindow.window);
         red += 0.001f;
         if (red > 1)
             red -= 1;
@@ -185,7 +154,7 @@ int main() {
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // present (send the current frame to the computer screen)
-        glfwSwapBuffers(window); // ??
+        glfwSwapBuffers(mainWindow.window); // ??
     }
     // Cleans up all the GLFW stuff
     glfwTerminate();
@@ -197,3 +166,4 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
