490423903b2801f9ed87c907f2f615fdf4f320e0
augustlewen <augustlewen@live.com>
2024-01-29 15:18:55 +0100

    updated the shaders and added glsl files

--- OpenGLEngine/OpenGLEngine.cpp
+++ OpenGLEngine/OpenGLEngine.cpp
@@ -49,26 +49,9 @@ int main() {
 
     // ----- Compile the Vertex Shader on the GPU -------
 
-    Shader vertexShader{"#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0", GL_VERTEX_SHADER};
-
-    Shader orangeShader{ "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "} \0", GL_FRAGMENT_SHADER};
-
-    Shader yellowShader{ "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-        "} \0", GL_FRAGMENT_SHADER };
+    Shader vertexShader{"vertexShader.glsl", GL_VERTEX_SHADER};
+    Shader orangeShader{"orangeFragmentShader.glsl", GL_FRAGMENT_SHADER};
+    Shader yellowShader{"yellowFragmentShader.glsl", GL_FRAGMENT_SHADER};
 
 
     Material orange{ vertexShader, orangeShader };
