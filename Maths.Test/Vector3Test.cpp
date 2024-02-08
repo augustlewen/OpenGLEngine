c0c9a7183fe0184a99784509fb0c7e239417b20e
augustlewen <augustlewen@live.com>
2024-02-08 11:37:53 +0100

    Added a bunch of bs

--- Maths.Test/Vector3Test.cpp
+++ Maths.Test/Vector3Test.cpp
@@ -1,30 +1,61 @@
 #include "pch.h"
 #include "../Maths/Vector3.h"
 
-TEST(Vector3_Constructor, AssignsAllComponents) {
+TEST(Vector3_Constructor, Assigns_All_Components) {
     Vector3 v(3, 4, 5);
     EXPECT_EQ(v.x, 3);
     EXPECT_EQ(v.y, 4);
     EXPECT_EQ(v.z, 5);
 }
 
-TEST(Vector3_EqualityComparison, Returns_True_For_Equal_Vectors) {
-    EXPECT_TRUE(Vector3(3, 4, 5) == Vector3(3, 4, 5));
+TEST(Vector3_EqualityComparison, Returns_True_For_Equal_Values) {
+    EXPECT_TRUE(Vector3(4, 5, 6) == Vector3(4, 5, 6));
 }
 
-// test and implement inequality comparison
-// Unit Tests
-// Test-Driven Development (First write the test, then the function, then clean up your code)
-TEST(Vector3_EqualityComparison, Returns_False_For_Inequal_Vectors) {
-    EXPECT_FALSE(Vector3(3, 4, 5) == Vector3(2, 4, 5));
-    EXPECT_FALSE(Vector3(3, 4, 5) == Vector3(3, 2, 5));
-    EXPECT_FALSE(Vector3(3, 4, 5) == Vector3(3, 4, 2));
-    EXPECT_FALSE(Vector3(3, 4, 5) == Vector3(2, 2, 2));
+TEST(Vector3_EqualityComparison, Returns_False_For_Inequal_Values) {
+    EXPECT_FALSE(Vector3(1, 2, 3) == Vector3(3, 2, 1));
 }
 
-//test and implement negate operator
+TEST(Vector3_InequalityComparison, Returns_True_For_Inequal_Values) {
+    EXPECT_TRUE(Vector3(1, 2, 3) != Vector3(3, 2, 1));
+}
 
-//TEST(Vector3_Negate, InvertsPositiveValues) {
-//    EXPECT_EQ(-Vector3(3,4,5), Vector3(-3,-4,-5));
-//    EXPECT_TRUE(true);
-//}
+TEST(Vector3_InequalityComparison, Returns_False_For_Equal_Values) {
+    EXPECT_FALSE(Vector3(1, 2, 3) != Vector3(1, 2, 3));
+}
+
+
+TEST(Vector3_Negate, Invert_Values) {
+    EXPECT_EQ(-Vector3(1, 2, 3), Vector3(-1, -2, -3));
+}
+
+TEST(Vector3_Addition, Add_Vector_To_Vector) {
+    EXPECT_EQ(Vector3(1, 2, 3) + Vector3(1, 2, 3), Vector3(2, 4, 6));
+}
+
+TEST(Vector3_ScalarMultiplication, Scales_All_Values) {
+    EXPECT_EQ(Vector3(3, 4, 5) * 2, Vector3(6, 8, 10));
+}
+
+TEST(Vector3_ScalarDivision, Inverse_Scale_All_Values) {
+    EXPECT_EQ(Vector3(6, 8, 10) / 2, Vector3(3, 4, 5));
+}
+
+TEST(Vector3_Subtraction, Subtracts_all_values) {
+    EXPECT_EQ(Vector3(9, 5, 7) - Vector3(1, 2, 3), Vector3(8, 3, 4));
+}
+
+TEST(Vector3_Magnitude, Returns_Magnitude_For_Vectors) {
+    EXPECT_FLOAT_EQ(Vector3(5, -3, 2).Magnitude(), sqrt(38));
+}
+
+
+TEST(Vector3_Normalize, Returns_Normalize_For_Vectors) {
+    Vector3 normalized = Vector3(5, -3, 2).Normalize();
+    Vector3 expected(5.0 / sqrt(38), -3.0 / sqrt(38), 2.0 / sqrt(38));
+    EXPECT_EQ(normalized, expected);
+}
+
+TEST(Vector3_SqrtMagnitude, Returns_Sqrt_Magnitude_For_Vectors) {
+    EXPECT_FLOAT_EQ(sqrt(Vector3(5, -3, 2).Magnitude()), sqrt(sqrt(38)));
+}
